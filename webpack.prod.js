6b7905b4e88f704856b99ac5593e6e6fc3431c9d
Andrea Cuneo <andreacuneo@Andreas-MacBook-Air.local>
2020-09-13 12:18:44 +0100

    update config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,16 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCssAssetsPlugin({})]
+    },
     output: {
         libraryTarget: 'var',
         library: 'Client'
@@ -20,7 +26,7 @@ module.exports = {
                 test: /\.scss$/,
                 // Chained loaders run in order from right to left
                 //   <---- 3rd ----   <--- 2nd ---   <--- 1st ---
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
             }
         ]
     },
@@ -28,6 +34,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css"})
     ]
 }
